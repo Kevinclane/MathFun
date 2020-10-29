26f1d439f3d6968c6205ec8304dfac119900f584
Kevin Lane <59802468+Kevinclane@users.noreply.github.com>
2020-10-29 14:01:45 -0600

    Updates

---
+++ precache-manifest.91088255c6be859f5e5914b4bfe39d96.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0750a1e6143a71d92c6d45f2e7766f",
+    "url": "/MathFun/index.html"
+  },
+  {
+    "revision": "06885aff47cdffff8d2d",
+    "url": "/MathFun/static/css/2.e712defa.chunk.css"
+  },
+  {
+    "revision": "69ba60f1a9840aa86884",
+    "url": "/MathFun/static/css/main.44ada0a3.chunk.css"
+  },
+  {
+    "revision": "06885aff47cdffff8d2d",
+    "url": "/MathFun/static/js/2.7c970ebf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MathFun/static/js/2.7c970ebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ba60f1a9840aa86884",
+    "url": "/MathFun/static/js/main.b2597d4d.chunk.js"
+  },
+  {
+    "revision": "d22f87ebfab421241d32",
+    "url": "/MathFun/static/js/runtime-main.475689b1.js"
+  },
+  {
+    "revision": "afd39b9c107e6884e59e516e89cb7ce6",
+    "url": "/MathFun/static/media/GreatJob.afd39b9c.gif"
+  },
+  {
+    "revision": "ee382ec7e89f49761a3e93d5f6465691",
+    "url": "/MathFun/static/media/Incorrect.ee382ec7.gif"
+  },
+  {
+    "revision": "086495c409e90d93d99b4861a147c868",
+    "url": "/MathFun/static/media/NavBanner.086495c4.jpg"
+  },
+  {
+    "revision": "9c91841fb8aa3a18501795ac7d163c1f",
+    "url": "/MathFun/static/media/borderBanner.9c91841f.png"
+  }
+]);
